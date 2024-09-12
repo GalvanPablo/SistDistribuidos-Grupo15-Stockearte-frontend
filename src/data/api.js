502c0c27d216205b88d70e08b1c07e98faaf78b9
Strychi8 <tomas2000T@gmail.com>
2024-09-12 20:19:12 -0300

    Elimino lineas de codigo basura

--- src/data/api.js
+++ src/data/api.js
@@ -6,14 +6,12 @@ export const API_TIENDA = {
     // GET_BY_ID: (id) => API_URL + `dispositivos/estacionamiento/${id}`,
 }
 
-<<<<<<< HEAD
 export const API_PRODUCTO = {
     ALTA: API_URL + 'producto/alta',
     LISTADO: API_URL + 'productos/listado',
-    
-=======
+}
+
 export const API_AUTH = {
     LOGIN: API_URL + 'auth/login',
     ROLES: ['ROLE_TIENDA','ROLE_CASA_CENTRAL']
->>>>>>> 8b0d4c1e428d1576f4a8ff28111de56eb76e1f26
 }
