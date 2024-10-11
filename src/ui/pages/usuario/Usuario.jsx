90e5244a3901f8296f9cd7a0ffd0c68dd4baf9f9
Pablo Galvan <103456849+GalvanPablo@users.noreply.github.com>
2024-10-11 11:59:19 -0300

    Importar Usuarios mediante .CSV

--- src/ui/pages/usuario/Usuario.jsx
+++ src/ui/pages/usuario/Usuario.jsx
@@ -8,9 +8,10 @@ import styles from './Usuario.module.css'
 import { Link } from 'react-router-dom'
 
 import { API_USUARIO } from '../../../data/api'
+import { ModalGeneric } from '../../components'
 
 const Usuario = () => {
-    
+
     const Item = ({ nombre, tienda, estado, id }) => (
         <tr className={styles.tabla__fila}>
             <td>{nombre}</td>
@@ -18,12 +19,12 @@ const Usuario = () => {
             <td>{estado}</td>
             <td className={styles.tabla__celdaAciones}>
                 <Link to={`/usuarios/usuario/${id}`} title='ver detalle'>
-                    <FontAwesomeIcon icon={faFilePen} className={styles.icono_detalles}/>
+                    <FontAwesomeIcon icon={faFilePen} className={styles.icono_detalles} />
                 </Link>
             </td>
         </tr>
     );
-    
+
     const [nombre, setNombre] = React.useState('')
     const [codigoTienda, setCodigoTienda] = React.useState('')
 
@@ -54,16 +55,67 @@ const Usuario = () => {
         obtenerListado();
     }, [nombre, codigoTienda]); // BUSCAR EN CADA CAMBIO DE FILTRO
 
+    const [modalOpen, setModalOpen] = React.useState(false);
+    const onCloseModal = () => {
+        setUsuariosImportar([]);
+        setModalOpen(false);
+    };
+
+    const [usuariosImportar, setUsuariosImportar] = React.useState([]);
+
+    const headers = ['email', 'clave', 'nombre', 'apellido', 'codigoTienda'];
+    const inputFile_onChange = (e) => {
+        const file = e.target.files[0];  // Obtiene el archivo del input
+        if (file) {
+            const reader = new FileReader();
+
+            reader.onload = function (e) {
+                const text = e.target.result;  // Lee el contenido del archivo como texto
+                const lines = text.split('\n');  // Divide el contenido en líneas
+
+                // Ignora la primera línea del archivo
+                const dataLines = lines.slice(1);
+
+                // Convertimos cada línea a un objeto
+                const data = dataLines.map(line => {
+                    const values = line.split(';').map(value => value.trim());  // Elimina \r y espacios extra
+                    const obj = {};
+                    headers.forEach((header, index) => {
+                        obj[header] = values[index];
+                    });
+                    return obj;
+                });
+
+                setUsuariosImportar(data);
+            };
+
+            reader.onerror = function (error) {
+                console.error("Error al leer el archivo:", error);
+            };
+
+            reader.readAsText(file);  // Lee el archivo como texto
+        }
+    }
+
+    const importar_onClick = () => {
+        alert('importando');
+    }
+
     return (
-     <>
-        <h1>Usuarios</h1>
-        <div className={styles.listado}>
+        <>
+            <h1>Usuarios</h1>
+            <div className={styles.listado}>
 
                 <div className={styles.toolbar}>
-                    <Link to={`/usuarios/nueva`} className={styles.nuevo}>Nuevo Usuario</Link>
+                    <div className={styles.btnNuevos}>
+                        <Link to={`/usuarios/nueva`} className={styles.nuevo}>Nuevo Usuario</Link>
+                        <button className={styles.nuevo} onClick={() => setModalOpen(true)}>
+                            <span>Importar</span>
+                        </button>
+                    </div>
                     <div className={styles.toolbar__filtro__container}>
-                        <input type="text" name="" id="" placeholder='Nombre' onChange={(e) => setNombre(e.target.value)}/>
-                        <input type="text" name="" id="" placeholder='Código de Tienda' onChange={(e) => setCodigoTienda(e.target.value)}/>
+                        <input type="text" name="" id="" placeholder='Nombre' onChange={(e) => setNombre(e.target.value)} />
+                        <input type="text" name="" id="" placeholder='Código de Tienda' onChange={(e) => setCodigoTienda(e.target.value)} />
                         <button onClick={obtenerListado}>
                             <FontAwesomeIcon icon={faFilter} />
                         </button>
@@ -85,14 +137,46 @@ const Usuario = () => {
                                 key={index}
                                 nombre={usuario.nombre}
                                 tienda={usuario.tienda}
-                                estado={usuario.habilitado?'Habilitado':'Deshabilitado'}
+                                estado={usuario.habilitado ? 'Habilitado' : 'Deshabilitado'}
                                 id={usuario.idUsuario}
                             />
                         ))}
                     </tbody>
                 </table>
             </div>
-     </>
+            <ModalGeneric isOpen={modalOpen} onClose={onCloseModal}>
+                <div className={styles.import__container}>
+                    <h2>Imporatar Usuarios</h2>
+                    <input type="file" id="fileInput" accept=".csv" onChange={inputFile_onChange} />
+                    <table className={`${styles.tabla} ${styles.tabla__modal}`}>
+                        <thead className={styles.tabla__encabezado}>
+                            <tr>
+                                <th>Email</th>
+                                <th>Clave</th>
+                                <th>Nombre</th>
+                                <th>Apellido</th>
+                                <th>Tienda</th>
+                            </tr>
+                        </thead>
+                        <tbody className={styles.tabla__cuerpo}>
+                            {usuariosImportar.map((usuario, index) => (
+                                <tr key={index}>
+                                    <td>{usuario.email}</td>
+                                    <td>{usuario.clave}</td>
+                                    <td>{usuario.nombre}</td>
+                                    <td>{usuario.apellido}</td>
+                                    <td>{usuario.codigoTienda}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                    <div className={styles.importar__buttons}>
+                        <button className={styles.btnImportarUsuarios} onClick={importar_onClick} disabled={!(usuariosImportar.length > 0)}>Importar</button>
+                        <button className={styles.btnCancelarImportarUsuarios} onClick={onCloseModal}>Cancelar</button>
+                    </div>
+                </div>
+            </ModalGeneric>
+        </>
     )
 }
 
